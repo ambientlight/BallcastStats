655ad23a36182df0fac5e1c6d5a3f9cb6a206bbc
ambientlight <primary.taras.vozniuk@gmail.com>
2019-05-21 01:53:05 +0800

    Revert "custom lambda resolver example"
    This reverts commit 0f2ffaa559e1f26291242ba873a77c6b0835e80e.

--- amplify/backend/function/ballcaststatsfuncs/src/index.js
+++
@@ -1,4 +0,0 @@
-exports.handler = function (event, context) { //eslint-disable-line
-  console.log(event);
-  context.done(null, event.arguments.msgP1 + event.arguments.msgP2); // SUCCESS with message
-};
