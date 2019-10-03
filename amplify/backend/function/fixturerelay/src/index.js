51655520f09bc13402e3a8156c228609e30f6df2
ambientlight <primary.taras.vozniuk@gmail.com>
2019-10-04 07:35:02 +0800

    base working fixturerelay flow

--- amplify/backend/function/fixturerelay/src/index.js
+++ amplify/backend/function/fixturerelay/src/index.js
@@ -5,13 +5,15 @@ var region = process.env.REGION
 var apiCorefootballGraphQLAPIIdOutput = process.env.API_COREFOOTBALL_GRAPHQLAPIIDOUTPUT
 var apiCorefootballGraphQLAPIEndpointOutput = process.env.API_COREFOOTBALL_GRAPHQLAPIENDPOINTOUTPUT
 
-Amplify Params - DO NOT EDIT */global.WebSocket = require('ws');
+Amplify Params - DO NOT EDIT */
+
+global.WebSocket = require('ws');
+const fs = require('fs');
 require('es6-promise').polyfill();
 require('isomorphic-fetch');
 const awsExports = require('./aws-exports');
 const AUTH_TYPE = require('aws-appsync/lib/link/auth-link').AUTH_TYPE;
 const AWSAppSyncClient = require('aws-appsync').default;
-const gql = require('graphql-tag');
 const AWS = require('aws-sdk');
 
 const url = awsExports.aws_appsync_graphqlEndpoint;
@@ -27,14 +29,40 @@ const client = new AWSAppSyncClient({
   disableOffline: true
 });
 
-const query = gql(`
-  query {
-    relay(msg:"Hi")
-  }
-`);
+const fixturesUpdatedMutation = require('./fixtureUpdated.mutation')
 
 exports.handler = (event, context) => {
-  return client.query({ query, fetchPolicy: 'network-only' })
-    .then(data => console.log(data))
-    .catch(console.error);
+  const records = event.Records.map(record => ({
+    new: AWS.DynamoDB.Converter.unmarshall(record.dynamodb.NewImage),
+    old: AWS.DynamoDB.Converter.unmarshall(record.dynamodb.OldImage)
+  }));
+
+  //console.log(process.env.ENV)
+  //console.log(process.env.REGION)
+
+  return client.mutate({ 
+    mutation: fixturesUpdatedMutation,
+    variables: {
+      fixtures: records.map(record => {
+        delete record.new['__typename']
+        delete record.new['createdAt']
+        delete record.new['updatedAt']
+        delete record.new['status#competitionId#homeTeamId#awayTeamId']
+        delete record.new['homeTeamLineup']
+        delete record.new['awayTeamLineup']
+        return record.new
+      })
+    },
+    fetchPolicy: 'no-cache'
+  })
+    .then(
+      ({data: { fixturesUpdated }}) => {
+        console.log(JSON.stringify(
+          fixturesUpdated.map(fx => 
+            `${fx.homeTeamName} ${fx.scores.current.home} : ${fx.awayTeamName} ${fx.scores.current.away} (min: ${fx.time.min}, sec: ${fx.time.sec}, id: ${fx.id})`
+          )
+        ))
+      },
+      console.error
+    )
 }
