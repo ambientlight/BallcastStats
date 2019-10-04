777afa6ab9567c80cb1fc6007e2c9aafded8ed3a
ambientlight <primary.taras.vozniuk@gmail.com>
2019-10-04 16:21:24 +0800

    example with a subscription

--- src/index.jsx
+++ src/index.jsx
@@ -12,11 +12,13 @@ import ruLocaleData from 'react-intl/locale-data/ru'
 addLocaleData(enLocaleData);
 addLocaleData(ruLocaleData);
 
-import Amplify from 'aws-amplify';
+import Amplify, { Auth } from 'aws-amplify';
 import awsmobile from './aws-exports';
 import Root from './Root'
 
 import { AWSAppSyncClient } from 'aws-appsync'
+import ApolloClient from 'apollo-client';
+import gql from 'graphql-tag'
 
 // configure Amplify
 Amplify.configure(awsmobile);
@@ -24,3 +26,41 @@ Amplify.configure(awsmobile);
 const root = document.createElement('div')
 document.body.appendChild(root)
 render(<Root />, root)
+
+/*
+// example to have subscriptions working
+const client = new AWSAppSyncClient({
+  url: awsmobile.aws_appsync_graphqlEndpoint,
+  region: awsmobile.aws_appsync_region,
+  auth: {
+    type: awsmobile.aws_appsync_authenticationType,
+    jwtToken: async () => (await Auth.currentSession()).getAccessToken().getJwtToken()
+  },
+  disableOffline: true
+});
+
+client.subscribe({
+  query: gql`
+    subscription ON_UPDATED_FIXTURES {
+      onUpdatedFixtures {
+        id,
+        status,
+        scores {
+          current {
+            home,
+            away
+          }
+        },
+        time {
+          min,
+          sec
+        },
+        homeTeamName,
+        awayTeamName
+      }
+    }
+  `
+}).subscribe({
+  next: value => console.log(value)
+})
+*/
