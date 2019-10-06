245d3e07e2d31207bcc8355e7187f5535d293b07
ambientlight <primary.taras.vozniuk@gmail.com>
2019-10-06 19:34:46 +0800

    add s3 support, relax few non-null constraints, larger query res for fixturerelay

--- amplify/backend/function/fixturerelay/src/fixtureUpdated.mutation.js
+++ amplify/backend/function/fixturerelay/src/fixtureUpdated.mutation.js
@@ -4,19 +4,102 @@ module.exports = gql`
   mutation FIXTURES_UPDATED($fixtures: [FixtureInput!]) {
     fixturesUpdated(fixtures: $fixtures){
       id,
+      modelVersion,
+      date,
+      competitionId,
+      homeTeamId,
+      awayTeamId,
+      startTs,
+      startTime,
       status,
       scores {
         current {
+          home,
+          away
+        },
+        ht {
+          home,
+          away
+        },
+        ft {
+          home,
+          away
+        },
+        et {
+          home,
+          away
+        },
+        pen {
           home,
           away
         }
       },
       time {
         min,
-        sec
-      }
+        sec,
+        ht_added,
+        ft_added,
+        fet_added,
+        set_added
+      },
       homeTeamName,
       awayTeamName,
+      competitionName,
+      homeTeamFormation,
+      awayTeamFormation,
+      homeTeamLineup {
+        playerId,
+        teamId,
+        playerName,
+        number,
+        position,
+        formationIdx,
+        captain,
+        posx,
+        posy,
+        playerIdSportmonks
+      },
+      awayTeamLineup {
+        playerId,
+        teamId,
+        playerName,
+        number,
+        position,
+        formationIdx,
+        captain,
+        posx,
+        posy,
+        playerIdSportmonks
+      },
+      events {
+        eventType,
+        playerId,
+        playerName,
+        relatedPlayerId,
+        relatedPlayerName,
+        minute,
+        extraMinute,
+        reason,
+        result {
+          home,
+          away
+        }
+        idSportmonks
+      },
+      comments {
+        important,
+        order,
+        goal,
+        minute,
+        extraMinute,
+        comment
+      },
+      idSportmonks,
+      dumpSportmonks {
+        bucket,
+        key,
+        region
+      }
     }
   }
 `
