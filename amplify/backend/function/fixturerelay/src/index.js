33196e02a438bc7612bb9fbf9a984b70c9ef57d7
ambientlight <primary.taras.vozniuk@gmail.com>
2019-11-03 23:18:42 +0800

    remove analytics test resources

--- amplify/backend/function/fixturerelay/src/index.js
+++ amplify/backend/function/fixturerelay/src/index.js
@@ -2,8 +2,6 @@
 You can access the following resource attributes as environment variables from your Lambda function
 var environment = process.env.ENV
 var region = process.env.REGION
-var apiCorefootballGraphQLAPIIdOutput = process.env.API_COREFOOTBALL_GRAPHQLAPIIDOUTPUT
-var apiCorefootballGraphQLAPIEndpointOutput = process.env.API_COREFOOTBALL_GRAPHQLAPIENDPOINTOUTPUT
 
 Amplify Params - DO NOT EDIT */
 
