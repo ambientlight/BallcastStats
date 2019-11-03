dd09988cfd396c43f89090aacb8988b56d57ae6b
ambientlight <primary.taras.vozniuk@gmail.com>
2019-11-03 20:56:39 +0800

    kinesis resource with kinesis test and fixturerelay with hackly params

--- amplify/backend/function/relay/src/index.js
+++ amplify/backend/function/relay/src/index.js
@@ -1,3 +1,9 @@
-exports.handler = function (event, context) {
+/* Amplify Params - DO NOT EDIT
+You can access the following resource attributes as environment variables from your Lambda function
+var environment = process.env.ENV
+var region = process.env.REGION
+var analyticsBallcaststatsKinesisStreamArn = process.env.ANALYTICS_BALLCASTSTATS_KINESISSTREAMARN
+
+Amplify Params - DO NOT EDIT */exports.handler = function (event, context) {
   context.done(null, event.arguments.msg);
 };
